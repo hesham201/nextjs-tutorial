6c07e8e12e54d6510fde43fe666d8a4b31b42d53
hesham112 <107617341+hesham201@users.noreply.github.com>
2024-02-13 04:58:41 +0500

    intial Commit

---
+++ app/about/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+const AboutPage = () => {
+  return (
+    <div>
+      AboutPage
+      <Link href={"/"} className="text-4xl">
+        Home Page
+      </Link>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/prisma-example/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PrismaExamplePage = () => {
+  return <div>PrismaExamplePage</div>;
+};
+
+export default PrismaExamplePage;
